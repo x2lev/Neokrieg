49cdb69aa41f13936ca9c8f8e384f7f978651076
x2lev <lev.kryvenko@gmail.com>
2024-02-10 22:19:42 -0700

    deleted heroscript

--- Assets/Scripts/Objects/Hitbox.cs
+++ Assets/Scripts/Objects/Hitbox.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class Hitbox
 {
-    public Vector2 center;
-    public Vector2 offset;
     public Vector2 launchAngle;
     public int priority;
-    public Hitbox(Vector2 center, Vector2 offset, Vector2 launchAngle, int priority)
+    public Hitbox(Vector2 launchAngle, int priority)
     {
-        this.center = center;
-        this.offset = offset;
         this.launchAngle = launchAngle;
         this.priority = priority;
     }
