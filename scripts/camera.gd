4daa668dcb5e904cffbbc605546ca5a8594acc7a
x2lev <lev.kryvenko@gmail.com>
2025-02-04 00:16:21 -0700

    implemented input buffer, added animations

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,11 +1,9 @@
 extends Camera2D
 
+@export var player: Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _ready():
 	pass
+
+func _process(_delta):
+	position.x = clamp(player.position.x/2, -144, 144)
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,9 +2,10 @@ extends CharacterBody2D
 
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -350.0
+@export var AIR_CONTROL = .10
 @onready var sprite = $AnimatedSprite2D
 var gravity = ProjectSettings.get_setting('physics/2d/default_gravity')
-var input_buffer = [
+var buffer = [
 	{
 		'dpad': Vector2.ZERO,
 		'buttons': {
@@ -17,35 +18,78 @@ var input_buffer = [
 		'frames': 0
 	}
 ]
+var crouching = false
+var blocking = false
+var attack = ''
+
+func _ready():
+	sprite.animation_finished.connect(_animation_finished)
+
+func _process(_delta):
+	sprite.play()
+	if attack:
+		sprite.animation = attack
+	elif is_on_floor():
+		sprite.flip_h = position.x > 0
+		if crouching:
+			sprite.animation = 'crouch'
+		elif buffer[0]['dpad'].x == 0:
+			sprite.animation = 'idle'
+		elif buffer[0]['dpad'].x < 0 == sprite.flip_h:
+			sprite.animation = 'run'
+		elif buffer[0]['dpad'].x > 0 == sprite.flip_h:
+			sprite.animation = 'run'
+			sprite.play_backwards()
+	else:
+		if velocity.y > 0:
+			sprite.animation = 'fall'
+		else:
+			sprite.animation = 'jump'
 
 func _physics_process(delta):
-	update_input_buffer()
-	
-	var input_string = str(input_buffer[0]['dpad'])
-	for button in input_buffer[0]['buttons']:
-		if input_buffer[0]['buttons'][button]:
-			input_string += ' ' + button
-	print(input_string, ' ', input_buffer[0]['frames'])
+	update_buffer(true)
+
+	crouching = buffer[0]['dpad'].y > 0 and is_on_floor() and not attack
+	blocking = velocity.x > 0 == sprite.flip_h and not attack
 
 	if not is_on_floor():
 		velocity.y += gravity * delta * (1.0 if velocity.y < 0 else 1.5)
+	
+	if not attack:
+		check_attacks()
 
-	if Input.is_action_just_pressed('up') and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction = Input.get_axis('left', 'right')
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	if is_on_floor():
-		sprite.flip_h = position.x > 0
+		if is_on_floor():
+			if buffer[0]['dpad'].y < 0 and buffer[0]['frames'] < 4:
+				velocity.y = JUMP_VELOCITY
+			
+			if not crouching:
+				if buffer[0]['dpad'].x:
+					velocity.x = buffer[0]['dpad'].x * SPEED
+				else:
+					velocity.x = move_toward(velocity.x, 0, SPEED)
+		else:
+			if buffer[0]['dpad'].x:
+				velocity.x = clamp(velocity.x + buffer[0]['dpad'].x * SPEED * AIR_CONTROL, -SPEED, SPEED)
+	
+	if is_on_floor() and attack:
+		velocity.x = 0
 
 	move_and_slide()
 
-func update_input_buffer():
-	var dpad = Input.get_vector('left', 'right', 'up', 'down')
+func check_attacks():
+	if is_on_floor():
+		if buffer[0]['buttons']['square']:
+			attack = 'attack1'
+		elif buffer[0]['buttons']['triangle']:
+			attack = 'attack2'
+		elif buffer[0]['buttons']['r1']:
+			attack = 'attack3'
+	else:
+		if buffer[0]['buttons']['square']:
+			attack = 'air_attack'
+
+func update_buffer(print_buffer):
+	var dpad = Input.get_vector('left', 'right', 'up', 'down').sign()
 	var buttons = {
 		'square': Input.is_action_pressed('square'),
 		'triangle': Input.is_action_pressed('triangle'),
@@ -59,9 +103,19 @@ func update_input_buffer():
 		'l3': Input.is_action_pressed('l3')
 	}
 	
-	if input_buffer[0]['dpad'] == dpad and input_buffer[0]['buttons'] == buttons:
-		input_buffer[0]['frames'] = min(999, input_buffer[0]['frames'] + 1)
+	if buffer[0]['dpad'] == dpad and buffer[0]['buttons'] == buttons:
+		buffer[0]['frames'] = min(999, buffer[0]['frames'] + 1)
 	else:
-		input_buffer.insert(0, {'dpad': dpad, 'buttons': buttons, 'frames': 1})
-		if len(input_buffer) > 10:
-			input_buffer.pop_back()
+		buffer.insert(0, {'dpad': dpad, 'buttons': buttons, 'frames': 1})
+		if len(buffer) > 10:
+			buffer.pop_back()
+
+	if print_buffer:
+		var input_string = str(buffer[0]['dpad'])
+		for button in buffer[0]['buttons']:
+			if buffer[0]['buttons'][button]:
+				input_string += ' ' + button
+		print(input_string, ' ', buffer[0]['frames'])
+
+func _animation_finished():
+	attack = ''
